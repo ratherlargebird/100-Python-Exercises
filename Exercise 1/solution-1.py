27984687ef128adc52b86ad70767f15b93befd8d
Callum Bird <callumbird@gmx.com>
2023-07-23 16:57:15 +0100

    First draft for question 1
    Working solution for the first question

--- Exercise 1/solution-1.py
+++ Exercise 1/solution-1.py
@@ -2,9 +2,12 @@
 # Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
 # between 2000 and 3200 (both included).
 # The numbers obtained should be printed in a comma-separated sequence on a single line.
-#
-# Hints: 
-# Consider use range(#begin, #end) method
 
 # Program must iterate between all numbers between 2000 and 3200 and check 2 parameters, divisible by 7 and not a mutiple of 5.
 
+a=[]
+for x in range(2000, 3201):
+    if x%7==0 and x%5!=0:
+        a.append(str(x))
+        
+print ','.join(a)
