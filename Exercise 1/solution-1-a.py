c2d302d2b095ca320e34fcf70d914ed4136b0b4b
Callum Bird <callumbird@gmx.com>
2023-07-23 20:10:53 +0100

    Update solution-1-a.py
    Further improvement by removing the need to delete terms from list.

--- Exercise 1/solution-1-a.py
+++ Exercise 1/solution-1-a.py
@@ -2,6 +2,7 @@
 
 a=0
 b=0
+c=0
 l=[]
 
 # find the first number that is divisible by 7
@@ -9,17 +10,27 @@ for x in range(2000, 3201):
     if x%7==0:
         a=x
         break
+        
+# Find the first number divisble by 5
 
-# add all numbers divisible by 7 to a list
-while a<3200:
-    l.append(str(a))
-    a += 7
-    
-# remove all numbers divisible by 5 from list
-for i in l:
-    if int(i)%5==0:
-        b = l.index(i)
-        del l[b::5]
+for y in range(a, 3201):
+    if y%5==0:
+        b=y
         break
+
+# Add numbers that are divisible by 7 to the list but skip every 5th term after first term divisible by 5
+        
+while a<3200:
+    if a<b:
+        l.append(str(a))
+        a += 7
+    else:
+        c += 1
+        if c != 4:
+            l.append(str(a))
+            a += 7
+        else:
+            c = 0
+            a += 7
         
 print(','.join(l))  
