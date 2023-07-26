388b34ef77f8aa503927e41ea3ea7609a7b5ecc3
Callum Bird <callumbird@gmx.com>
2023-07-26 11:35:59 +0100

    Create solution-4.py
    Solution for question 4.

---
+++ Exercise 4/solution-4.py
@@ -0,0 +1,15 @@
+# Question:
+# Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number.
+# Suppose the following input is supplied to the program:
+# 34,67,55,33,12,98
+# Then, the output should be:
+# ['34', '67', '55', '33', '12', '98']
+# ('34', '67', '55', '33', '12', '98')
+
+
+l = list((int(x) for x in input("Enter a sequence of numbers seperated by a comma: ").split(",")))
+
+t = tuple(l)
+
+print(l)
+print(t)
